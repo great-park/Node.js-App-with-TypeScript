f43f3d83a62035862a87300c5da4b3daa15e1bba
great-park <cksgh1735@naver.com>
2022-09-07 16:57:43 +0900

    convert js to ts

---
+++ database.ts
@@ -0,0 +1,40 @@
+/**
+ * 간단한 API 테스트를 위한 sample 데이터
+ */
+
+export const users = [
+  {
+    email: "kim@gmail.com",
+    password: "KimPassword",
+  },
+  {
+    email: "park@gmail.com",
+    password: "JackPassword",
+  },
+];
+
+//jwt없이 조회 가능 posts
+export const publicPosts = [
+  {
+    title: "Post 1",
+    content: "Post 1 is free",
+  },
+  {
+    title: "Post 2",
+    content: "Post 2 is free",
+  },
+];
+
+//jwt있어야 조회 가능 posts
+export const privatePosts = [
+  {
+    title: "Post 3",
+    content: "Post 3 is private",
+  },
+  {
+    title: "Post 4",
+    content: "Post 4 is private",
+  },
+];
+
+
